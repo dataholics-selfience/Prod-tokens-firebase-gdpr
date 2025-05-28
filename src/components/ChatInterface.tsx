3bea326ea9897ee21dd070300ca0b44a7c52fe64
dataholics-selfience <48730751+dataholics-selfience@users.noreply.github.com>
2025-05-28 10:52:12 -0300

    Update ChatInterface.tsx

--- src/components/ChatInterface.tsx
+++ src/components/ChatInterface.tsx
@@ -14,8 +14,8 @@ interface ChatInterfaceProps {
   currentChallenge: ChallengeType | undefined;
 }
 
-const MESSAGE_TOKEN_COST = 3;
-const STARTUP_LIST_TOKEN_COST = 30;
+const MESSAGE_TOKEN_COST = 2;
+const STARTUP_LIST_TOKEN_COST = 50;
 
 const ChatInterface = ({ messages, addMessage, toggleSidebar, isSidebarOpen, currentChallenge }: ChatInterfaceProps) => {
   const [input, setInput] = useState('');
